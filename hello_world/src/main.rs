2138cb31ac2726c76fc1e85ef5a785b3ab91d052
Andres <andresnrx@gmail.com>
2016-09-15 02:20:30 -0700

    added special format example

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -20,6 +20,19 @@ struct Complex {
 
 struct List(Vec<i32>);
 
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
 impl Display for Structure {
     fn fmt(&self, f: &mut Formatter) -> Result {
         let Structure(number) = *self;
@@ -67,6 +80,35 @@ impl Display for List {
         }
 
         write!(f, "]")
+    }
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(f,
+               "{}: {:.3}°{} {:.3}°{}",
+               self.name,
+               self.lat.abs(),
+               lat_c,
+               self.lon.abs(),
+               lon_c)
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f,
+               "RGB ({}, {}, {}) {:#X}{:>0width$X}{:>0width$X}",
+               self.red,
+               self.green,
+               self.blue,
+               self.red,
+               self.green,
+               self.blue,
+               width = 2)
     }
 }
 
@@ -145,6 +187,46 @@ fn try_example() {
     println!("{}", v);
 }
 
+fn special_fmt_example() {
+    for city in [City {
+                     name: "Dublin",
+                     lat: 53.347778,
+                     lon: -6.259722,
+                 },
+                 City {
+                     name: "Oslo",
+                     lat: 59.95,
+                     lon: -6.259722,
+                 },
+                 City {
+                     name: "Vancouver",
+                     lat: 49.25,
+                     lon: -123.1,
+                 }]
+        .iter() {
+        println!("{}", city);
+    }
+
+    for color in [Color {
+                      red: 128,
+                      green: 255,
+                      blue: 90,
+                  },
+                  Color {
+                      red: 0,
+                      green: 3,
+                      blue: 254,
+                  },
+                  Color {
+                      red: 0,
+                      green: 0,
+                      blue: 0,
+                  }]
+        .iter() {
+        println!("{}", color)
+    }
+}
+
 fn main() {
     hello_world_example();
     comment_example();
@@ -152,4 +234,5 @@ fn main() {
     debug_format_example();
     display_example();
     try_example();
+    special_fmt_example();
 }
