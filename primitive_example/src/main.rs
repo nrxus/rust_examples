6dcba4a9e3fed165f3418a785ca6d0a76839a6f6
Andres <andresnrx@gmail.com>
2016-09-17 20:53:19 -0700

    added tuples example

--- primitive_example/src/main.rs
+++ primitive_example/src/main.rs
@@ -11,6 +11,36 @@ fn primitives_operations_examples() {
     println!("One million is written as {}", 1_000_000u32);
 }
 
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn tuples_examples() {
+    let long_tuple = (1u8, 2u16, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+    println!("long tuple first vale: {}", long_tuple.0);
+    println!("lon tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix)
+}
+
 fn main() {
     primitives_operations_examples();
+    tuples_examples();
 }
