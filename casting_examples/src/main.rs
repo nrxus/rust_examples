1db86c49fe377a14674bd5d04621b3d211e479f7
Andres <andresnrx@gmail.com>
2016-09-18 22:36:00 -0700

    added alias examples

--- casting_examples/src/main.rs
+++ casting_examples/src/main.rs
@@ -39,8 +39,25 @@ fn inferance_examples() {
     println!("{:?}", vec);
 }
 
+type NanoSecond = u64;
+type Inch = u64;
+
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+
+fn alias_examples() {
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    println!("{} nanoseconds + {} inches = {} unit?",
+             nanoseconds,
+             inches,
+             nanoseconds + inches);
+}
+
 fn main() {
     basic_casting_examples();
     literals();
     inferance_examples();
+    alias_examples();
 }
