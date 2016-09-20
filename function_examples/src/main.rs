52dc9a4782cf13993f5e72a00c5cb8cda8d217d0
Andres <andresnrx@gmail.com>
2016-09-20 01:10:10 -0700

    added basic closures examples

--- function_examples/src/main.rs
+++ function_examples/src/main.rs
@@ -75,6 +75,27 @@ fn method_examples() {
     pair.destroy();
 }
 
+fn basic_closures_examples() {
+    fn function(i: i32) -> i32 {
+        i + 1
+    }
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred = |i| i + 1;
+
+    let i = 1;
+    println!("function: {}", function(i));
+    println!("annotated closure: {}", closure_annotated(i));
+    println!("inferred closure: {}", closure_inferred(i));
+
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+    let professor_x = "Charles Xavier";
+    let print = || println!("Professor X's name is: {}", professor_x);
+    print();
+}
+
 fn main() {
     method_examples();
+    basic_closures_examples();
 }
