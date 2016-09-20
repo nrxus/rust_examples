22f3c54dc46e2eb27ddd05cb54d1270a1e202320
Andres <andresnrx@gmail.com>
2016-09-19 23:56:24 -0700

    add if/else example

--- flow_control_examples/src/main.rs
+++ flow_control_examples/src/main.rs
@@ -1,3 +1,25 @@
+fn if_else_example() {
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n = if n < 10 && n > -10 {
+        println!(", and is a small number, increase ten-fold");
+        10 * n
+    } else {
+        println!(", and is a big number, reduce by two");
+        n / 2
+    };
+
+    println!("{} -> {}", n, big_n);
+}
+
 fn main() {
-    println!("Hello, world!");
+    if_else_example();
 }
