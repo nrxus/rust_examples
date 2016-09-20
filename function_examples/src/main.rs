a262a22190cd0aa6c90b7b2c5237331a4d6d8dcf
Andres <andresnrx@gmail.com>
2016-09-20 01:18:35 -0700

    added capture examples for closures

--- function_examples/src/main.rs
+++ function_examples/src/main.rs
@@ -95,7 +95,36 @@ fn basic_closures_examples() {
     print();
 }
 
+fn capturing_examples() {
+    use std::mem;
+
+    let color = "green";
+
+    let print = || println!("color: {}", color);
+    print();
+    print();
+
+    let mut count = 0;
+    let mut inc = || {
+        count += 1;
+        println!("count: {}", count);
+    };
+
+    inc();
+    inc();
+
+    let movable = Box::new(3);
+
+    let consume = || {
+        println!("movable: {:?}", movable);
+        mem::drop(movable);
+    };
+
+    consume();
+}
+
 fn main() {
     method_examples();
     basic_closures_examples();
+    capturing_examples();
 }
